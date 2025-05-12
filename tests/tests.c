bf57c790e89bf0c7260fc3f35d4b0e43bcad24f3
Konstantin <wintermindset@yandex.ru>
2025-05-12 17:21:23 +0300

    [CLEANUP] `tests.c` clean up changes.

--- tests/tests.c
+++ tests/tests.c
@@ -109,7 +109,7 @@ int main() {
         }
         free(A); free(B); free(C);
     }
-	printf("✅ All 40 tests passed.\n");
+    printf("✅ All 40 tests passed.\n");
     printf("Timing results (seconds):\n");
     printf("Naive:\t\t\t%.5f\n", Time[13] / 40.0);
     for (int i = 0; i < 13; ++i) {
